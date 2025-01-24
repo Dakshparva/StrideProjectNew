eb81ddcc65baf77b3478dee2925b82775ce4959c
Dakshathkumar N <101206655+Dakshparva@users.noreply.github.com>
2025-01-24 15:05:45 +0530

    changed done on mobile view

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import Accordion from "../components/Accordian";
 import "./styles.css";
 import ProjectsMobileData from "../constants/ProjectsMobileData";
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
 
 function ProjectPage() {
   const { id } = useParams();
@@ -12,14 +14,20 @@ function ProjectPage() {
   if (!project) return <h1>Project Not Found</h1>;
 
   return (
-    <div style={{ padding: "20px" }}>
-      <button className="back-button" onClick={() => navigate(-1)}>
-        ← Back
+    <>
+    <div style={{width:"100%",backgroundColor:"#007bff",display:"flex",justifyContent:"space-between",borderRadius:"10px"}}>
+     <button className="back-button" onClick={() => navigate(-1)}>
+        <ArrowBackIosOutlinedIcon/> Back
       </button>
-      <h1>{project?.name}</h1>
+      <h1 style={{marginRight:"25px",color:"white"}}>{project?.name}</h1>
+      <span style={{display:"flex",justifyContent:"center",alignItems:"center",marginRight:"8px"}}><ErrorOutlineOutlinedIcon /></span>
+      
+      </div>
+    <div style={{ padding: "20px" }}>
       <p>{project?.description}</p>
       <Accordion items={project?.items} />
     </div>
+    </>
   );
 }
 
